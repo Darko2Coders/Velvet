66d61c2b21bebb757ae92e73fd04642dc1f7d406
Alberto González Acosta <alberto@2coders.com>
2023-01-16 13:55:00 +0000

    [SETUP] Initial setup
    · Add options for Chrome execution in pipeline.

--- src/test/java/com/velvet/AppTest.java
+++ src/test/java/com/velvet/AppTest.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AppTest 
 {
     @Test
     public void simpleTest() throws InterruptedException
     {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.es");
         Thread.sleep(2000);
         driver.findElement(By.id("L2AGLb")).click();
